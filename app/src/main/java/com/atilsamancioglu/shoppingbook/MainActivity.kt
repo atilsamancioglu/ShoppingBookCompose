b1e7f4362aab6e5eb0addda312fd62e90127d26d
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-07 15:39:15 +0300

    fab added

--- app/src/main/java/com/atilsamancioglu/shoppingbook/MainActivity.kt
+++ app/src/main/java/com/atilsamancioglu/shoppingbook/MainActivity.kt
@@ -4,13 +4,30 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.atilsamancioglu.shoppingbook.model.Item
 import com.atilsamancioglu.shoppingbook.ui.theme.ShoppingBookTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +37,21 @@ class MainActivity : ComponentActivity() {
         setContent {
             ShoppingBookTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Box(modifier = Modifier.padding(innerPadding)) {
+
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppingBookTheme {
-        Greeting("Android")
+
     }
 }
---
+++ app/src/main/java/com/atilsamancioglu/shoppingbook/screens/ListScreen.kt
@@ -0,0 +1,69 @@
+package com.atilsamancioglu.shoppingbook.screens
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.atilsamancioglu.shoppingbook.model.Item
+
+@Composable
+fun ItemList(itemList: List<Item>) {
+    Scaffold(topBar = {},
+        floatingActionButtonPosition = FabPosition.End,
+        floatingActionButton = {
+            FAB {
+                //navigation -> add_screen
+            }
+        } , content = { innerPadding ->
+            LazyColumn( contentPadding = innerPadding,
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(color = MaterialTheme.colorScheme.primaryContainer)
+            ) {
+                items(itemList) {
+                    ItemRow(item=it)
+                }
+            }
+        })
+}
+
+
+@Composable
+fun ItemRow(item:Item) {
+    Column(modifier = Modifier
+        .fillMaxWidth()
+        .background(color = MaterialTheme.colorScheme.primaryContainer)
+        .clickable {
+            //navigation -> details_screen
+        }
+    ) {
+        Text(text=item.itemName,
+            style = MaterialTheme.typography.displayMedium,
+            modifier = Modifier.padding(2.dp),
+            fontWeight = FontWeight.Bold
+        )
+    }
+}
+
+@Composable
+fun FAB(onClick : () -> Unit) {
+    FloatingActionButton(onClick = onClick) {
+        Icon(Icons.Filled.Add,"Floating button")
+    }
+}
