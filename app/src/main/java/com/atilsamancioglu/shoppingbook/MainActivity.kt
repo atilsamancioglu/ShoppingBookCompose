94a3ffeba3bbd9f5ab3042801861ea6cf8a6b88c
Atil Samancioglu <samancioglu.atil@gmail.com>
2024-10-08 11:14:22 +0300

    detail screen added

--- app/src/main/java/com/atilsamancioglu/shoppingbook/MainActivity.kt
+++ app/src/main/java/com/atilsamancioglu/shoppingbook/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.atilsamancioglu.shoppingbook
 
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
@@ -11,11 +13,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.Button
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -23,8 +27,15 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.atilsamancioglu.shoppingbook.model.Item
@@ -48,10 +59,12 @@ class MainActivity : ComponentActivity() {
 
 
 
+
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     ShoppingBookTheme {
-
+        
     }
 }
---
+++ app/src/main/java/com/atilsamancioglu/shoppingbook/screens/DetailsScreen.kt
@@ -0,0 +1,79 @@
+package com.atilsamancioglu.shoppingbook.screens
+
+import android.graphics.BitmapFactory
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.res.imageResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.atilsamancioglu.shoppingbook.R
+import com.atilsamancioglu.shoppingbook.model.Item
+
+@Composable
+fun DetailScreen(item: Item, deleteFunction: () -> Unit) {
+    Box(modifier =
+    Modifier
+        .fillMaxSize()
+        .background(color = MaterialTheme.colorScheme.primaryContainer)
+        , contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text=item.itemName,
+                style = MaterialTheme.typography.displayLarge,
+                modifier = Modifier.padding(2.dp),
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+
+            val imageBitmap = item.image?.let { byteArray ->
+                BitmapFactory.decodeByteArray(byteArray,0,byteArray.size).asImageBitmap()
+            }
+
+
+            Image(bitmap = imageBitmap ?: ImageBitmap.imageResource(id = R.drawable.selectimage),
+                contentDescription = item.itemName,
+                modifier = Modifier
+                    .padding(16.dp)
+                    .size(300.dp, 200.dp)
+            )
+
+            Text(text=item.storeName ?: "",
+                style = MaterialTheme.typography.displaySmall,
+                modifier = Modifier.padding(2.dp),
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+
+            Text(text=item.price ?: "",
+                style = MaterialTheme.typography.displaySmall,
+                modifier = Modifier.padding(2.dp),
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                textAlign = TextAlign.Center
+            )
+
+            Button(onClick = {
+                deleteFunction()
+            }) {
+                Text("Delete")
+            }
+        }
+    }
+}
